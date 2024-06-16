b31e3175d8cff1528e4c7aba0beabe0e366b3a55
dev974 <dev.galima@outlook.fr>
2024-06-16 19:56:57 +0200

    initale commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {App} from "./app";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'sandbox-jointjs-imageprocessor';
+
+  ngOnInit(): void {
+    let app = new App();
+  }
 }
